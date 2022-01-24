3eff4ed48544a76847efc180e168d5c78c4ad557
yufan-lu <yufanlu98@outlook.com>
2022-01-23 19:37:56 -0800

    blog frontend

---
+++ config/prod.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  BASE_API: '"http://120.76.134.233:8888/api/"'
+}
---
+++ src/components/card/CardMe.vue
@@ -0,0 +1,65 @@
+<template>
+  <el-card>
+    <h1 class="me-author-name">云水遥遥</h1>
+    <div class="me-author-description">
+      <span>&nbsp; Violent delights have violent ends.</span>
+    </div>
+    <div class="me-author-tool">
+      <i @click="showTool(qq)" :title="qq.title" class="iconfont icon-qq"></i>
+      <i @click="showTool(bilibili)" :title="bilibili.title" class="iconfont icon-bilibili-fill"></i>
+    </div>
+  </el-card>
+
+</template>
+
+<script>
+  export default {
+    name: 'CardMe',
+    data() {
+      return {
+        qq: {title: 'QQ', message: '604944359'},
+        bilibili: {
+          title: '哔哩哔哩',
+          message: '<a target="_blank" href="https://space.bilibili.com/233990145">https://space.bilibili.com/233990145</a>'
+        }
+      }
+    },
+    methods: {
+      showTool(tool) {
+        this.$message({
+          duration: 0,
+          showClose: true,
+          dangerouslyUseHTMLString: true,
+          message: '<strong>' + tool.message + '</strong>'
+        });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .me-author-name {
+    text-align: center;
+    font-size: 30px;
+    border-bottom: 1px solid #5FB878;
+  }
+
+  .me-author-description {
+    padding: 8px 0;
+  }
+
+  .me-icon-job {
+    padding-left: 16px;
+  }
+
+  .me-author-tool {
+    text-align: center;
+    padding-top: 10px;
+  }
+
+  .me-author-tool i {
+    cursor: pointer;
+    padding: 4px 10px;
+    font-size: 30px;
+  }
+</style>
